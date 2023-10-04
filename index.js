eaf9d857993a04e07e205edc4239eb5a94d36a92
Nawwal Mohammed <102904919+Nawwalicious@users.noreply.github.com>
2023-10-04 13:00:21 +0530

    CORS

--- index.js
+++ index.js
@@ -7,11 +7,7 @@ const app=express();
 
 const PORT=process.env.PORT||7000;
 
-const getOrigin = () => {
-    return `http://localhost:5173/`;
-}
-
-app.use(cors({ origin: getOrigin() })); 
+app.use(cors({ origin: "http://localhost:5173", })); 
 //To FIX The Error
 //{ blocked by CORS policy: No ['Access-Control-Allow-Origin'] header is present on the requested resource }
 
